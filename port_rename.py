8a94f638ef29deb4b6651a9c19b48ac98056feb2
Dan Sinema <dsinema@gmail.com>
2021-02-16 13:23:15 -0700

    Update port_rename.py
    Update to pivot to use the the interface monitor code

--- port_rename.py
+++ port_rename.py
@@ -36,29 +36,62 @@ Manifest = {
 class Agent(NAE):
 
     def __init__(self):
-
-        # Setup the Monitor to trigger when a port authentication happens
-        uri1 = '/rest/v10.04/system/interfaces/*/port_access_clients?attributes=client_state'  # The attribute we want to monitor, in this case it is port_access_clients
-#       uri1 = '/rest/v1/system/interfaces/*?attributes=link_state&filter=type:system'
-        self.m1 = Monitor(              # Declare our monitor
-            uri1,                       # URI of the attribute we are monitoring
-            'Port access Client Name')  # Name of monitor
-
+    # I'm going to bring over the interface_link_state_monitor script and modify. Having to do some tricky stuff because we can only use v1 REST interface with NAE scripts :(
+         # Interface status
+        uri1 = '/rest/v1/system/interfaces/*?attributes=link_state' + \
+            '&filter=type:system'
+        self.m1 = Monitor(
+            uri1,
+            'Interface Link status')
         self.r1 = Rule('Link Went Down')
-        self.r1.condition('transition {} from "up" to "down"', [self.m1])   # We only care about the transition to "authenticated"
-#       self.r1.action(self.action_port_auth)                               # TODO: Modify to function that will handle this condition
+        self.r1.condition('transition {} from "up" to "down"', [self.m1])
+        self.r1.action(self.action_interface_down)
 
-# Reset policy status when port is up
-#       self.r2 = Rule('Port enabled administratively')
-#       self.r2.condition('transition {} from "down" to "up"', [self.m1])   # TODO: Build condition based upon possible values
-#       self.r2.action(self.action_port_up)                                 # TODO: Modify to function that will handle this condition
+        # Reset agent status when link is up
+        self.r2 = Rule('Link Came UP')
+        self.r2.condition('transition {} from "down" to "up"', [self.m1])
+        self.r2.action(self.action_interface_up)
 
-#    def action_port_auth(self, event):                                      # THESE ARE PLACEHOLDERS and WILL BE CHANGED       
-#       ActionSyslog(
-#            'Port {} is disabled administratively',
-#            [self.params['port_id']])
-#        ActionCLI("show lldp configuration {}", [self.params['port_id']])
-#       ActionCLI("show interface {} extended", [self.params['port_id']])
-#       if self.get_alert_level() != AlertLevel.CRITICAL:
-#           self.set_alert_level(AlertLevel.CRITICAL)
-#       self.logger.debug("### Critical Callback executed")
+        # variables
+        self.variables['links_down'] = ''
+
+    def action_interface_down(self, event):
+        self.logger.debug("================ Down ================")
+        label = event['labels']
+        self.logger.debug('label: [' + label + ']')
+        _, interface_id = label.split(',')[0].split('=')
+        self.logger.debug('interface_id - ' + interface_id)
+        links_down = self.variables['links_down']
+        self.logger.debug('links_down before: ['
+                          + links_down + ']')
+        if (interface_id + ':') not in links_down:
+            links_down = links_down + interface_id + ':'
+            self.variables['links_down'] = links_down
+            ActionSyslog('Interface ' + interface_id + ' Link gone down')
+            ActionCLI("show lldp configuration " + interface_id)
+            ActionCLI("show interface " + interface_id + " extended")
+            if self.get_alert_level() != AlertLevel.CRITICAL:
+                self.set_alert_level(AlertLevel.CRITICAL)
+        self.logger.debug('links_down after: ['
+                          + links_down + ']')
+        self.logger.debug("================ /Down ================")
+
+    def action_interface_up(self, event):
+        self.logger.debug("================ Up ================")
+        label = event['labels']
+        self.logger.debug('label: [' + label + ']')
+        _, interface_id = label.split(',')[0].split('=')
+        self.logger.debug('interface_id - ' + interface_id)
+        links_down = self.variables['links_down']
+        self.logger.debug('links_down before: ['
+                          + links_down + ']')
+        if (interface_id + ':') in links_down:
+            links_down = links_down.replace((interface_id + ':'), '')
+            self.variables['links_down'] = links_down
+            self.logger.debug('links_down after: ['
+                              + links_down + ']')
+            ActionSyslog('Interface ' + interface_id + ' Link came up')
+            if not links_down:
+                if self.get_alert_level() is not None:
+                    self.remove_alert_level()
+        self.logger.debug("================ /Up ================")
